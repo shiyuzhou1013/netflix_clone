b4f2d688e2493f4cef04868623d783034be451a3
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-08-25 10:41:24 +1200

    nodemon

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
 app.listen(3000, () => {
   console.log("Server started at http://localhost:3000");
 });
