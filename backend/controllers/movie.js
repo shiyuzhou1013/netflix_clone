df497287d5f77e0947098b791d60657495699a8e
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-08-25 18:18:27 +1200

    routes for movies set up

--- backend/controllers/movie.js
+++ backend/controllers/movie.js
@@ -21,7 +21,55 @@ export async function getMovieTrailers(req, res) {
       `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`
     );
 
-    res.json({ success: true, trailors: data.results });
+    res.status(200).json({ success: true, trailors: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+export async function getMovieDetails(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+    );
+
+    res.status(200).json({ success: true, content: data });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+export async function getSimilarMovies(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`
+    );
+
+    res.status(200).json({ success: true, similars: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+export async function getMoviesByCategory(req, res) {
+  const { category } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`
+    );
+
+    res.status(200).json({ success: true, content: data.results });
   } catch (error) {
     if (error.message.includes("404")) {
       return res.status(404).send(null);
--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { getTrendingMovie, getMovieTrailers } from "../controllers/movie.js";
+import {
+  getTrendingMovie,
+  getMovieTrailers,
+  getMovieDetails,
+  getSimilarMovies,
+  getMoviesByCategory,
+} from "../controllers/movie.js";
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
 router.get("/:id/trailers", getMovieTrailers);
+router.get("/:id/details", getMovieDetails);
+router.get("/:id/similars", getSimilarMovies);
+router.get("/:category", getMoviesByCategory);
 
 export default router;
