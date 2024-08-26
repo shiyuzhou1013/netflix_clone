96585f2dd77998ee2f5b131d7edc222a2e0ed745
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-08-26 18:41:43 +1200

    visitor page part1 set up

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { ChevronRight } from "lucide-react";
 
 const AuthScreen = () => {
+  const [email, setEmail] = useState("");
+
   return (
     <div className="hero-bg relative">
       {/* NavBar */}
@@ -15,6 +18,31 @@ const AuthScreen = () => {
           Sign In
         </Link>
       </header>
+
+      {/* hero section */}
+      <div className="flex flex-col items-center justify-center text-center py-40 text-white max-w-6xl mx-auto">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4">
+          Unlimited movies, TV shows, and more
+        </h1>
+        <p className="text-lg mb-4">Watch anywhere. Cancel anytime</p>
+        <p className="mb-4">
+          Ready to watch? Enter your email to create or restart your membership
+        </p>
+
+        <form className="flex flex-col md:flex-row gap-4 w-1/2">
+          <input
+            type="email"
+            placeholder="Email address"
+            className="p-2 rounded flex-1 bg-black/80 border border-gray-700"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button className=" text-xl lg:text-2xl px-2 lg:px-6 py-1 lg:py-2 rounded flex justify-center items-center bg-red-600">
+            Get Started
+            <ChevronRight className="size-8 md:size:10" />
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
