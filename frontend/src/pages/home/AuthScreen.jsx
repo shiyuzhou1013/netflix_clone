a97d679c9c68a4614934b4d7ea3b978480b0696c
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-08-26 19:59:11 +1200

    visitor page part4 set up

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -121,6 +121,43 @@ const AuthScreen = () => {
           </div>
         </div>
       </div>
+
+      {/* separator */}
+      <div className="h-2 w-full bg-[#232323]" aria-hidden="true" />
+
+      {/* 3rd section */}
+      <div className="py-10 bg-black text-white">
+        <div className="flex max-w-6xl mx-auto items-center justify-between md:flex-row flex-col px-4 md:px-2 ">
+          {/* left side */}
+          <div className="flex-1 text-center md:text-left">
+            <h2 className="text-4xl md:text-5xl font-extrabold mb-4">
+              Watch everywhere
+            </h2>
+            <p className="text-lg md:text-xl">
+              Stream unlimited movies and TV shows on your phone, tablet,
+              laptop, and TV
+            </p>
+          </div>
+
+          {/* rigth side */}
+          <div className="flex-1 relative overflow-hidden">
+            <img
+              src="/device-pile.png"
+              alt="Device set"
+              className="mt-4 z-10 relative"
+            />
+            <video
+              className="absolute top-2 left-1/2 -translate-x-1/2 h-4/6 z-0 max-w-[63%]"
+              playsInline
+              autoPlay={true}
+              muted
+              loop
+            >
+              <source src="/video-devices.m4v" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
