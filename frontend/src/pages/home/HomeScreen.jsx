7238ad91f37766bcd3a386ca90074072fc289a6b
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-08-27 20:16:30 +1200

    home screen part1 set up

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -5,6 +5,12 @@ const HomeScreen = () => {
   return (
     <div className="relative h-screen text-white bg-black">
       <NavBar />
+      <img
+        src="/extraction.jpg"
+        alt="Hero img"
+        className="absolute top-0 left-0 h-full object-cover -z-50"
+      />
+      <div className="absolute top-0 left-0 h-full bg-black/50 -z-50" />
     </div>
   );
 };
