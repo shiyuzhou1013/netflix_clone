7165904545e3753658b99e3bbe19b88ef6777e09
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-08-28 10:48:19 +1200

    home screen banner section set up

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,16 +1,50 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
+import { Play, Info } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const HomeScreen = () => {
   return (
-    <div className="relative h-screen text-white bg-black">
+    <div className="relative h-screen text-white">
       <NavBar />
       <img
         src="/extraction.jpg"
         alt="Hero img"
         className="absolute top-0 left-0 w-full h-full object-cover -z-50"
       />
-      <div className="absolute top-0 left-0 h-full bg-black/50 -z-50" />
+      <div
+        className="absolute top-0 left-0 w-full h-full bg-black/50 -z-50"
+        aria-hidden="true"
+      />
+      <div className="absolute top-0 left-0  w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32">
+        <div className="bg-gradient-to-b from-black via-transparent to-transparent absolute top-0 left-0  w-full h-full -z-10" />
+        <div className="max-w-2xl">
+          <h1 className="mt-4 text-6xl font-extrabold text-balance">
+            Extraction
+          </h1>
+          <p className="mt-2 text-lg">2014 | 18+</p>
+          <p className="mt-4 text-lg">
+            Chris Hemsworth stars in this nonstop action-thriller with Rudhraksh
+            Jaiswal, Randeep Hooda and Golshifteh Farahani.
+          </p>
+        </div>
+        <div className="flex mt-8">
+          <Link
+            to="/watch/123"
+            className="bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded mr-4 flex items-center"
+          >
+            <Play className="size-6 mr-2 fill-black" />
+            Play
+          </Link>
+          <Link
+            to="/watch/123"
+            className="bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded flex items-center"
+          >
+            <Info className="size-6 mr-2" />
+            More Info
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
