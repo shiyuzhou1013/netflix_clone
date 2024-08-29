4c78eae6c4e54260524075e88ba89997ba930f3d
shiyuzhou1013 <szho231@aucklanduni.ac.nz>
2024-08-29 15:54:45 +1200

    setting up package.json and update readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-readme
+# Netflix Clone
+
+# Tech Stack: React.js, Node.js, Express.js, MongoDB, Tailwind
+
+# Authentication with JWT
+
+# Features
+
+1. Responsive Design
+2. Browse Movies and TV Shows
+3. Search for Movies, TV Shows, and Actors
+4. Browse Details - Trailor, Content, Similars
+5. Landing Page for Guests
+6. Browse Search History
+
+# Setup .env file
+
+PORT=3000
+MONGO_URI=your_mongo_uri
+NODE_ENV=development
+JWT_SECRET=your_jwt_secre
+TMDB_API_KEY=your_tmdb_api_key
+
+# Run the app locally
+
+npm run build
+
+# Start teh app
+
+npm run start
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/authRoutes.js";
 import movieRoutes from "./routes/movieRoutes.js";
@@ -12,6 +13,7 @@ import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,6 +22,13 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
+
+if (ENV_VARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
